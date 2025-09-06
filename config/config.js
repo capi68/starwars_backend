add9c7b1040d9d8ad49329bb9d32a9c3d3de6b00
yersi alviarez <yersialviarez@gmail.com>
2025-09-05 21:54:37 -0500

    modificacion de config.js para subir db a nube

--- config/config.js
+++ config/config.js
@@ -2,18 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
   }
 };
-
