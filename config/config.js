7fcc6089cc7fb65374253c2f58615512dfb76d23
yersi alviarez <yersialviarez@gmail.com>
2025-09-08 11:04:30 -0500

    separar PORT de DB_PORT

--- config/config.js
+++ config/config.js
@@ -2,11 +2,26 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: "postgres",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "starwars_db",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {}
   },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: "postgres",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
